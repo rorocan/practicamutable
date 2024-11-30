f88b5ceaf4d8dfe849446ee57ad1e221d8da09dd
Rodrigo Rojas <rorocanrojas1998@gmail.com>
2024-11-30 09:55:46 -0500

    mutable

---
+++ server/02-map.js
@@ -0,0 +1,10 @@
+const letters = ['🍔','🍟','🥤'];
+
+const newArray = letters.map(item => item + '++');
+// const newArray = [];
+// for (let index = 0; index < letters.length; index++) {
+//     const element = letters[index];
+//     newArray.push(element + '++');
+// }
+console.log('original', letters);
+console.log('new', newArray);
